2ae2593587a1a4772e537d751f0c593f0d047479
hoangnhit <Hoangnh09022003@gmail.com>
2024-12-08 20:37:12 +0700

    Code part1

---
+++ Hotel-Manager/src/View/ViewHotel.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import java.util.*;
+
+/**
+ *
+ * @author nguyenhuuhoang
+ */
+public class ViewHotel {
+    private Scanner  scanner;
+    
+    public ViewHotel() {
+        scanner = new Scanner(System.in);
+    }
+}
